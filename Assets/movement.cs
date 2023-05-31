d582a2c6c9291b5ea9536bc38db38aa906ce3ef1
Dionysos199 <stimelkh@stud.h-da.de>
2023-05-31 22:23:34 +0200

    test scene photon

---
+++ Assets/movement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float walkSpeed=4;
+    public float maxVelocityChange = 10;
+    // Start is called before the first frame update
+    private Vector2 input;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        input.Normalize();
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce(calculateMovement(walkSpeed), ForceMode.VelocityChange);
+    }
+    Vector3 calculateMovement(float _speed)
+    {
+        Vector3 targetVelocity = new Vector3(input.x, 0, input.y);
+        targetVelocity = transform.TransformDirection(targetVelocity);
+        targetVelocity *= _speed;
+
+        Vector3 velocity = rb.velocity;
+        if (input.magnitude>.5f)
+        {
+            Vector3 velocityChange = targetVelocity - velocity;
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+            return velocityChange;
+        }
+       else
+
+        {
+            return new Vector3();
+        }
+    }
+}

366eb13c25d54de3108ade38242d7520a2e95f7a
PhilippDaube96 <philipp.daube@stud.h-da.de>
2023-06-28 19:35:57 +0200

    Scene Change and URP Pipeline
    I added 2 Scripts to create a scene change and modified the whale model I also changed the pipeline to URP

---
+++ Assets/SceneController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
+
+    public void NextLevel()
+    {
+        Debug.Log("NextLevel called");
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}

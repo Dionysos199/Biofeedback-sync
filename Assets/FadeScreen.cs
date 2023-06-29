768e260e382f8df323822bf6ba09c51a49c23494
PhilippDaube96 <philipp.daube@stud.h-da.de>
2023-06-29 17:19:09 +0200

    Added a new script for fadescreen

--- Assets/FadeScreen.cs
+++ Assets/FadeScreen.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.AffordanceSystem.Receiver.Primitives;
 
 public class FadeScreen : MonoBehaviour
 {
+    public float fadeDuration = 2;
+    public Color fadeColor;
+    private Renderer _renderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _renderer = GetComponent<Renderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Fade(float alphaIn, float alphaOut)
     {
         
+    }
+        
+    public IEnumerator FadeRoutine(float alphaIn, float alphaOut)
+    {
+        float timer = 0;
+        while( timer<= fadeDuration )
+        {
+            Color newColor = fadeColor;
+            newColor.a = Mathf.Lerp(alphaIn, alphaOut, timer / fadeDuration);
+
+          //  _renderer.material.SetColor();
+
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+
     }
 }

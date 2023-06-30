78d806d37cc4cb9e6796ecf7c7a209d989773af9
PhilippDaube96 <philipp.daube@stud.h-da.de>
2023-06-30 18:22:53 +0200

    Modified PortalShader

---
+++ Assets/PortalModifier.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalModifier : MonoBehaviour
+{
+    private Renderer _renderer;
+
+    public float StartBrightness = 3.89f;
+    public float EndBrightness = 0.24f;
+    public float TriggerDistance =10f;
+    public float DecreaseValue = 0.5f;
+
+    public Transform player;
+    public Transform SceneTransitionObject;
+   
+
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    private void Update()
+    {
+        PlayerDistance();
+    }
+    public void PlayerDistance()
+    {
+
+       if ( Vector3.Distance(player.position, SceneTransitionObject.position) <= TriggerDistance)
+        {
+            _renderer.material.SetFloat("Brightness", StartBrightness);
+
+           Debug.Log("Brightness gets decreased");
+           float delta = (StartBrightness - DecreaseValue) * Time.deltaTime;
+           delta *= Time.deltaTime;
+
+           // StartBrightness -= delta;
+
+            //float t = 1 - (Vector3.Distance(player.position, SceneTransitionObject.position) / 10);
+            //float brightness = Mathf.Lerp(StartBrightness, EndBrightness, t);
+            //_renderer.material.SetFloat("Brightness", brightness);
+        }
+       
+    }
+
+   
+}

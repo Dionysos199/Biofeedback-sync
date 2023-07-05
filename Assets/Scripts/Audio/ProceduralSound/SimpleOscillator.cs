4eb5968e111f1ee817c0f4ce8eddfc4258dab44d
Florian Beck <florian.f.beck@web.de>
2023-07-05 11:27:47 +0200

    Simplified oscillator implementation

---
+++ Assets/Scripts/Audio/ProceduralSound/SimpleOscillator.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public abstract class SimpleOscillator : MonoBehaviour
+{
+    [SerializeField, Range(16.35f, 7902.13f)] private float frequency = 261.62f; // middle C
+
+    protected double _phase;
+    private int _sampleRate;
+
+    private void Awake()
+    {
+        _sampleRate = AudioSettings.outputSampleRate; // 48 kHz default
+    }
+
+    private void OnAudioFilterRead(float[] data, int channels)
+    {
+        // calculate how much the phase should change after each sample
+        double phaseIncrement = frequency / _sampleRate;
+
+        for (int sample = 0; sample < data.Length; sample += channels)
+        {
+            // get value of phase on a sine wave
+            float value = GetPhaseValue();
+
+            // increment phase value for next iteration
+            _phase = (_phase + phaseIncrement) % 1;
+
+            // populate all channels with the values
+            for (int channel = 0; channel < channels; channel++)
+            {
+                data[sample + channel] = value;
+            }
+        }
+    }
+
+    protected abstract float GetPhaseValue();
+}
--- Assets/Scripts/Audio/ProceduralSound/SimpleSineGenerator.cs
+++ Assets/Scripts/Audio/ProceduralSound/SimpleSineGenerator.cs
@@ -1,38 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class SimpleSineGenerator : MonoBehaviour
+public class SimpleSineGenerator : SimpleOscillator
 {
     [SerializeField, Range(0, 1)] private float amplitude = 0.5f;
-    [SerializeField, Range(16.35f, 7902.13f)] private float frequency = 261.62f; // middle C
 
-    private double _phase;
-    private int _sampleRate;
-
-    private void Awake()
+    protected override float GetPhaseValue()
     {
-        _sampleRate = AudioSettings.outputSampleRate; // 48 kHz default
-    }
-
-    private void OnAudioFilterRead(float[] data, int channels)
-    {
-        // calculate how much the phase should change after each sample
-        double phaseIncrement = frequency / _sampleRate;
-
-        for (int sample = 0; sample < data.Length; sample += channels)
-        {
-            // get value of phase on a sine wave
-            float value = Mathf.Sin((float) _phase * 2 * Mathf.PI) * amplitude;
-
-            // increment phase value for next iteration
-            _phase = (_phase + phaseIncrement) % 1;
-
-            // populate all channels with the values
-            for (int channel = 0; channel < channels; channel++)
-            {
-                data[sample + channel] = value;
-            }
-        }
+        return Mathf.Sin((float) _phase * 2 * Mathf.PI) * amplitude;
     }
 }

410a0062eaa548cb35fa7ea68a5321e7cb8a03f8
PhilippDaube96 <philipp.daube@stud.h-da.de>
2023-07-06 17:42:59 +0200

    fixed bubble shader

--- Assets/Scripts/Narrator.cs
+++ Assets/Scripts/Narrator.cs
@@ -9,7 +9,7 @@ public class Narrator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SpeechManager.StartReadMessage("Hello and welcome to our experience! I will guide you threw this little adventure to your innerself ");
+        SpeechManager.StartReadMessage("Hello and welcome to our experience! I will guide you through this little adventure to your innerself");
     }
 
     // Update is called once per frame
